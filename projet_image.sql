5e09efe1aac67fac2f971c0df85a2f512b80b478
anthyllide <gonzalez.axa@wanadoo.fr>
2015-04-20 22:09:23 +0200

    ajout import bdd

---
+++ projet_image.sql
@@ -0,0 +1,140 @@
+﻿-- phpMyAdmin SQL Dump
+-- version 4.1.14
+-- http://www.phpmyadmin.net
+--
+-- Client :  127.0.0.1
+-- Généré le :  Lun 20 Avril 2015 à 21:58
+-- Version du serveur :  5.6.17
+-- Version de PHP :  5.5.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Base de données :  `projet_image`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `images`
+--
+
+CREATE TABLE IF NOT EXISTS `images` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `title` varchar(255) COLLATE utf8_bin NOT NULL,
+  `description` text COLLATE utf8_bin NOT NULL,
+  `filename` varchar(200) COLLATE utf8_bin NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `filename` (`filename`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=18 ;
+
+--
+-- Contenu de la table `images`
+--
+
+INSERT INTO `images` (`id`, `title`, `description`, `filename`) VALUES
+(13, 'espace', 'L''infini', 'espace.jpg'),
+(14, 'La Route', 'Jolie route', 'route.jpg'),
+(15, '', '', 'ville.jpg'),
+(16, '', '', 'planeur.jpg'),
+(17, '', '', 'voilier.jpg');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `login` varchar(40) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(32) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `role` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `user`
+--
+
+INSERT INTO `user` (`id`, `login`, `password`, `role`) VALUES
+(1, 'Cestmoi', 'monmotdepasse', 0),
+(3, 'editeur@me.com', 'editeur', 1),
+(4, 'administrateur', 'administrateur', 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_action`
+--
+
+CREATE TABLE IF NOT EXISTS `user_action` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `slung` varchar(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=3 ;
+
+--
+-- Contenu de la table `user_action`
+--
+
+INSERT INTO `user_action` (`id`, `name`, `slung`) VALUES
+(0, 'Déconnexion', 'logout'),
+(1, 'Administration', 'admin'),
+(2, 'Upload', 'upload');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_permission`
+--
+
+CREATE TABLE IF NOT EXISTS `user_permission` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `min_role_id` int(11) NOT NULL,
+  `action_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_bin AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `user_permission`
+--
+
+INSERT INTO `user_permission` (`id`, `min_role_id`, `action_id`) VALUES
+(1, 0, 0),
+(2, 1, 1),
+(3, 2, 2);
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(20) NOT NULL,
+  `level` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=4 ;
+
+--
+-- Contenu de la table `user_role`
+--
+
+INSERT INTO `user_role` (`id`, `name`, `level`) VALUES
+(1, 'Aucun', 0),
+(2, 'Editeur', 1),
+(3, 'Administrateur', 2);
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
